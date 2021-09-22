3eb02d0191db357d702e423d824dcf225b8bcdd3
nedimehuljic <nedim.mehuljic@hotmail.com>
2021-09-23 01:56:26 +0200

    Repo10

---
+++ ex01/src/index.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+var myDog = {
+    species: "Pit Bull",
+    name: "Roky",
+    legs: 4;
+};
+
+var friends = {bestFriend1: "Micky", bestFriend2: "John"};
+
+function myFunction(myObj){
+    return myObj;
+}
+
+// Oncly change code above this line
+
+console.log(myFunction());
+module.exports = { myPet, myFunction};
---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+var myClothes = {
+    hat: "Red",
+    shirt: "Blue",
+    shoes: "Addidas"
+};
+
+function myFunction(myObj){
+    var hatValue = myObje.hat;
+    var shirtValue = myObj.shirt;
+    var shoesValue = myObj.shoes;
+}
+// Oncly change code above this line
+
+console.log(myFunction(myObj));
+module.exports = myFunction(myClothes);
